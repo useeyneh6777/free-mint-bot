78db65c64a80f550cc0b9d3153543071a9af0aa7
useeyneh6777 <111541448+useeyneh6777@users.noreply.github.com>
2026-04-12 20:17:37 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,4 +19,119 @@ async function sendTelegram(message) {
 async function checkEth() {
   try {
     const url = "https://eth-mainnet.g.alchemy.com/nft/v3/" + ALCHEMY_KEY + "/getMintedNfts";
-    const res = await axios.get(url, { params: { limit: 20, order​​​​​​​​​​​​​​​​
+    const res = await axios.get(url, { params: { limit: 20, orderBy: "BLOCK_TIME_DESC" } });
+    const nfts = res.data && res.data.nfts ? res.data.nfts : [];
+    for (var i = 0; i < nfts.length; i++) {
+      var nft = nfts[i];
+      var contract = nft.contract && nft.contract.address ? nft.contract.address : "";
+      var name = nft.contract && nft.contract.name ? nft.contract.name : "Unknown Collection";
+      var key = "eth:" + contract;
+      if (seen.has(key)) continue;
+      seen.add(key);
+      var xSearch = "https://x.com/search?q=" + encodeURIComponent(name) + "&src=typed_query";
+      var msg =
+        "🆓 <b>Free Mint Alert! (Ethereum)</b>\n\n" +
+        "🖼 <b>" + name + "</b>\n" +
+        "📄 Contract: <code>" + contract + "</code>\n\n" +
+        "🔗 Mint Links:\n" +
+        "• <a href='https://mint.fun/eth/" + contract + "'>mint.fun</a>\n" +
+        "• <a href='https://zora.co/collect/eth:" + contract + "'>Zora</a>\n" +
+        "• <a href='https://opensea.io/assets/ethereum/" + contract + "'>OpenSea</a>\n" +
+        "• <a href='https://etherscan.io/address/" + contract + "'>Etherscan</a>\n" +
+        "• <a href='" + xSearch + "'>Search on X 𝕏</a>";
+      await sendTelegram(msg);
+      await new Promise(function(r) { setTimeout(r, 1000); });
+    }
+  } catch (err) {
+    console.error("ETH error: " + err.message);
+  }
+}
+
+async function checkBase() {
+  try {
+    const url = "https://base-mainnet.g.alchemy.com/nft/v3/" + ALCHEMY_KEY + "/getMintedNfts";
+    const res = await axios.get(url, { params: { limit: 20, orderBy: "BLOCK_TIME_DESC" } });
+    const nfts = res.data && res.data.nfts ? res.data.nfts : [];
+    for (var i = 0; i < nfts.length; i++) {
+      var nft = nfts[i];
+      var contract = nft.contract && nft.contract.address ? nft.contract.address : "";
+      var name = nft.contract && nft.contract.name ? nft.contract.name : "Unknown Collection";
+      var key = "base:" + contract;
+      if (seen.has(key)) continue;
+      seen.add(key);
+      var xSearch = "https://x.com/search?q=" + encodeURIComponent(name) + "&src=typed_query";
+      var msg =
+        "🆓 <b>Free Mint Alert! (Base)</b>\n\n" +
+        "🖼 <b>" + name + "</b>\n" +
+        "📄 Contract: <code>" + contract + "</code>\n\n" +
+        "🔗 Mint Links:\n" +
+        "• <a href='https://mint.fun/base/" + contract + "'>mint.fun</a>\n" +
+        "• <a href='https://zora.co/collect/base:" + contract + "'>Zora</a>\n" +
+        "• <a href='https://opensea.io/assets/base/" + contract + "'>OpenSea</a>\n" +
+        "• <a href='https://basescan.org/address/" + contract + "'>Basescan</a>\n" +
+        "• <a href='" + xSearch + "'>Search on X 𝕏</a>";
+      await sendTelegram(msg);
+      await new Promise(function(r) { setTimeout(r, 1000); });
+    }
+  } catch (err) {
+    console.error("Base error: " + err.message);
+  }
+}
+
+async function checkApeChain() {
+  try {
+    const rpc = "https://rpc.apechain.com/http";
+    const blockRes = await axios.post(rpc, {
+      jsonrpc: "2.0", method: "eth_blockNumber", params: [], id: 1
+    });
+    const blockNum = parseInt(blockRes.data.result, 16);
+    const logsRes = await axios.post(rpc, {
+      jsonrpc: "2.0",
+      method: "eth_getLogs",
+      params: [{
+        fromBlock: "0x" + (blockNum - 100).toString(16),
+        toBlock: "latest",
+        topics: ["0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef",
+                 "0x0000000000000000000000000000000000000000000000000000000000000000"]
+      }],
+      id: 2
+    });
+    const logs = logsRes.data.result || [];
+    for (var i = 0; i < logs.length; i++) {
+      var log = logs[i];
+      var contract = log.address;
+      var key = "ape:" + contract;
+      if (seen.has(key)) continue;
+      seen.add(key);
+      var xSearch = "https://x.com/search?q=" + encodeURIComponent(contract) + "&src=typed_query";
+      var msg =
+        "🆓 <b>Free Mint Alert! (ApeChain)</b>\n\n" +
+        "📄 Contract: <code>" + contract + "</code>\n\n" +
+        "🔗 Links:\n" +
+        "• <a href='https://apescan.io/address/" + contract + "'>ApeScan</a>\n" +
+        "• <a href='https://opensea.io/assets/apechain/" + contract + "'>OpenSea</a>\n" +
+        "• <a href='" + xSearch + "'>Search on X 𝕏</a>";
+      await sendTelegram(msg);
+      await new Promise(function(r) { setTimeout(r, 1000); });
+    }
+  } catch (err) {
+    console.error("ApeChain error: " + err.message);
+  }
+}
+
+async function checkMints() {
+  console.log("Checking all chains...");
+  await checkEth();
+  await checkBase();
+  await checkApeChain();
+}
+
+async function start() {
+  console.log("Bot starting...");
+  if (!TELEGRAM_TOKEN || !CHAT_ID) { process.exit(1); }
+  await sendTelegram("🤖 Free Mint Bot is live!\nWatching Ethereum, Base + ApeChain every 15 mins.");
+  await checkMints();
+  setInterval(checkMints, POLL_INTERVAL);
+}
+
+start();
